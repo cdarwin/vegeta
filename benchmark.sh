ebf9f9749e026e21f93e71f1ca2a937a3b3aa368
Jason Thigpen <darwin@senet.us>
2015-07-12 11:41:53 -0700

    Add initial benchmark wrapper script

---
+++ benchmark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[[ -z $TARGET ]] && TARGET="${1:-http://localhost:8080/}"
+[[ -z $DURATION ]] && DURATION="${2:-5s}"
+[[ -z $RATE ]] && RATE="${3:-10}"
+[[ -z $TIMEOUT ]] && TIMEOUT="${4:-10s}"
+[[ -z $REPORTER ]] && REPORTER="${5:-json}"
+
+echo "GET ${TARGET}" | /go/bin/vegeta attack -duration=${DURATION} -rate=${RATE} -timeout=${TIMEOUT} | /go/bin/vegeta report -reporter=${REPORTER}
